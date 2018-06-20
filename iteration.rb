cd758ad41aacc986ca0caf560d4662d7c9737080
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-20 01:06:08 +0000

    automatically backed up by learn

--- iteration.rb
+++ iteration.rb
@@ -1,10 +1,7 @@
 cart_item_prices = [12.43, 19.99, 3.49, 75.00]
 
-count = 1
-
 cart_item_prices.each do |price|
-
-  puts "Item #{count}: #{price}"
-  count += 1
-
-end 
+price_with_tax = price * 1.17
+tax_included << price_with_tax
+end
+# => [12.43, 19.99. 3.49, 75.0 ]
