5aba8dbb4b04a6736c5baf747222c4a440e01098
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-20 01:32:25 +0000

    automatically backed up by learn

--- reducing.rb
+++ reducing.rb
@@ -1,10 +1,10 @@
 cart_item_prices = [12.43, 19.99, 3.49, 75.00]
-big_ticket_prices = []
+
+total = 0
 
 cart_item_prices.each do |price|
-  if price >=15
-    big_ticket_prices << price
-  end
+  total += price
+  
 end
 
-puts big_ticket_prices 
+puts big_ticket_prices
