7690a07eb4d101b621bb81a0052ea8346d472431
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-20 01:29:53 +0000

    automatically backed up by learn

---
+++ reducing.rb
@@ -0,0 +1,10 @@
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+big_ticket_prices = []
+
+cart_item_prices.each do |price|
+  if price >=15
+    big_ticket_prices << price
+  end
+end
+
+puts big_ticket_prices 
